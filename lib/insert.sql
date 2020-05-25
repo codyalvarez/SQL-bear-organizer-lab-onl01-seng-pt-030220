2af506066050eb1f30da9960ab139063903c9d1c
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-25 01:46:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@
 "INSERT INTO bears (name, age, breed) VALUES ("Melissa", 42, "Flying Bitch")"
 "INSERT INTO bears (name, age, breed) VALUES ("Grinch", 60, "Melissa's Grandmother")"
 "INSERT INTO bears (name, age, breed) VALUES ("Wendy", 100, "Bear")"
-"INSERT INTO bears (name, age, breed) VALUES ("NULL", 40, "Killed Tim")"
+INSERT INTO bears (name, age, breed) VALUES ("NULL", 40, "Killed Tim")
