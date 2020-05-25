95cf732e6b54eb3d89afa2fb8927cfa715e236cb
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-25 04:56:56 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age)
+  VALUES ("Mr.Chocolate", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Rowdy", 24);
+INSERT INTO bears (name, age)
+  VALUES ("Tabitha", 14);
+INSERT INTO bears (name, age)
+  VALUES ("Sergeant Brown", 50);
+INSERT INTO bears (name, age)
+  VALUES ("Melissa", 42);
+INSERT INTO bears (name, age)
+  VALUES ("Grinch", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Wendy", 100);
+INSERT INTO bears (name, age)
+  VALUES (NULL, NULL);
