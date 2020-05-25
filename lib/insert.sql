2463d8e3a71f8d13112aa70cd4d5db6040ae4d67
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-25 01:57:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, age, breed)
-  VALUES ("Mr.Chocolate", 60, "Black Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Rowdy", 24, "Space Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Tabitha", 14, "Care Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Sergeant Brown", 16, "Army Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Melissa", 42, "Momma Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Grinch", 60, "Mean Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("Wendy", 100, "Bear");
-INSERT INTO bears (name, age, breed)
-  VALUES ("NULL", 40, "Killed Tim");
+INSERT INTO bears (name, age)
+  VALUES ("Mr.Chocolate", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Rowdy", 24);
+INSERT INTO bears (name, age)
+  VALUES ("Tabitha", 14);
+INSERT INTO bears (name, age)
+  VALUES ("Sergeant Brown", 50);
+INSERT INTO bears (name, age)
+  VALUES ("Melissa", 42);
+INSERT INTO bears (name, age)
+  VALUES ("Grinch", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Wendy", 100);
+INSERT INTO bears (name, age)
+  VALUES ("NULL", 40);
