fd014dfdef9d7b1cc8e6ce2899d021c9cef8114b
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-25 02:25:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO bears (name, age, gender)
-  VALUES ("Mr.Chocolate", 60, "male");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Rowdy", 24, "male");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Tabitha", 14, "female");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Sergeant Brown", 50, "female");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Melissa", 42, "female");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Grinch", 60, "female");
-INSERT INTO bears (name, age, gender)
-  VALUES ("Wendy", 100, "female");
+INSERT INTO bears (name, age)
+  VALUES ("Mr.Chocolate", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Rowdy", 24);
+INSERT INTO bears (name, age)
+  VALUES ("Tabitha", 14);
+INSERT INTO bears (name, age)
+  VALUES ("Sergeant Brown", 50);
+INSERT INTO bears (name, age)
+  VALUES ("Melissa", 42);
+INSERT INTO bears (name, age)
+  VALUES ("Grinch", 60);
+INSERT INTO bears (name, age)
+  VALUES ("Wendy", 100);
 INSERT INTO bears (name, age)
   VALUES (NULL, NULL);
